70289f7e7d03d71edd1b55fe63d97ed4619a61b3
ZACTF426 <60770707+ZACTF426@users.noreply.github.com>
2020-02-22 17:21:09 -0500

    switch Arcade Drive
    code updated during chat with EMW

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,12 +19,14 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
+import edu.wpi.first.wpilibj.DigitalInput;
 
 
 public class Robot extends TimedRobot {
   private DifferentialDrive m_myRobot;
-  private XboxController gamepad;
+  //Define controllers. 
+  private XboxController gamepadDrive;
+  private XboxController gamepadOperator;
 
   
   private WPI_TalonSRX leftMotorControllerCIM1;
@@ -38,13 +40,20 @@ public class Robot extends TimedRobot {
   private WPI_VictorSPX conveyorMotorCIM1;
   private WPI_VictorSPX conveyorMotorCIM2;
   private SpeedControllerGroup conveyorMotorGroup;  
+
+  private DigitalInput colorWheelArmLowerLimit;
+  private DigitalInput colorWheelArmUpperLimit;
   private WPI_VictorSPX colorWheelDrive;
+  private WPI_VictorSPX colorWheelArm;
+
+
+  //private ColorWheelSystem colorWheelSystem;
 
   @Override
   public void robotInit() {
    
-
-  //Set up the Talons according to the spreadsheet here:  https://docs.google.com/spreadsheets/d/1-l5YZYubWAp52MwDntlmeQ8fC4OWeWa1os5C94XbTL8/edit?usp=sharing
+//The system inputs/outputs are arranged according to the spreadsheet here:  https://docs.google.com/spreadsheets/d/1-l5YZYubWAp52MwDntlmeQ8fC4OWeWa1os5C94XbTL8/edit?usp=sharing
+//Set up the drive motor controllers 
       leftMotorControllerCIM1 = new WPI_TalonSRX(0);
       leftMotorControllerCIM2 = new WPI_TalonSRX(1);
       leftMotorGroup = new SpeedControllerGroup(leftMotorControllerCIM1,leftMotorControllerCIM2);
@@ -53,44 +62,64 @@ public class Robot extends TimedRobot {
       rightMotorControllerCIM2 = new WPI_TalonSRX(3);
       rightMotorGroup = new SpeedControllerGroup(rightMotorControllerCIM1,rightMotorControllerCIM2);
 
-      
+//Create a differential drive system using the left and right motor groups
+      m_myRobot = new DifferentialDrive(leftMotorGroup, rightMotorGroup);
+      m_myRobot.setRightSideInverted(false);
+
+//Set up the two Xbox controllers. The drive is for driving, the operator is for all conveyor and color wheel controls
+      gamepadDrive = new XboxController(0);
+      gamepadOperator = new XboxController(1);
+ 
       leftMotorGroup.setInverted(true);
+//Set up conveyor motor controllers
       conveyorMotorCIM1 = new WPI_VictorSPX(6);
       conveyorMotorCIM2 = new WPI_VictorSPX(7);
       conveyorMotorGroup = new SpeedControllerGroup(conveyorMotorCIM1,conveyorMotorCIM2);
 
+//Set up the color wheel system motor controllers
       colorWheelDrive = new WPI_VictorSPX(8);
+      colorWheelArm = new WPI_VictorSPX(9);     
 
-    //Create a differential drive using the left motor group and right motor groups.
-      m_myRobot = new DifferentialDrive(leftMotorGroup, rightMotorGroup);
-      m_myRobot.setRightSideInverted(false);
-      gamepad = new XboxController(0);
- 
+//Set up the color wheel arm limit switches
+      colorWheelArmLowerLimit = new DigitalInput(4);
+      colorWheelArmUpperLimit = new DigitalInput(5);
   }
 
   @Override
   public void teleopPeriodic() {
   
-
-    //Set the drive motors according to the coordinates of the left joystick
-    double leftY = gamepad.getY(Hand.kLeft);
-    double leftX = gamepad.getX(Hand.kLeft);
-    double rightY = gamepad.getY(Hand.kRight);
-    double rightX = gamepad.getX(Hand.kRight)*-1;
+  //**********DRIVE CONTROL**********//
+  //Set the drive motors according to the coordinates of the right joystick on the drive controller
+    
+    double leftY = gamepadDrive.getY(Hand.kLeft);
+    double rightX = gamepadDrive.getX(Hand.kRight)*-0.7;
     
 
-    m_myRobot.arcadeDrive(rightY,rightX);
+    m_myRobot.arcadeDrive(leftY,rightX);
+
+
+//**********SMART DASHBOARD CONTROL**********//
+//This line sends things to the smart dashboard. We can use this to get any information we might want from the system.
     SmartDashboard.putNumber("leftMotor", leftMotorControllerCIM1.get());
     SmartDashboard.putNumber("rightMotor", rightMotorControllerCIM1.get());
     SmartDashboard.putNumber("conveyorMotor", conveyorMotorCIM1.get());
-    //If button X is pressed...
-    if(gamepad.getXButton()){
+    SmartDashboard.putNumber("colorWheelArm", colorWheelArm.get());
+    SmartDashboard.putNumber("colorWheelDrive", colorWheelDrive.get());
+    SmartDashboard.putBoolean("colorWheelArmUpperLimit", colorWheelArmLowerLimit.get());
+    SmartDashboard.putBoolean("colorWheelArmLowerLimit", colorWheelArmUpperLimit.get());
+
+
+//**********CONVEYOR CONTROL**********//
+
+//left button is full intake, bottom button is full stop, right button is full dump
+//If button X is pressed on the operator control...
+    if(gamepadOperator.getXButton()){
       //Set the conveyor to full forward
       conveyorMotorGroup.set(1.0);
     } 
     else
     //if button B is pressed
-    if(gamepad.getBButton()){
+    if(gamepadOperator.getBButton()){
       //Set the conveyor to full backward
       conveyorMotorGroup.set(-1.0);
     }
@@ -99,14 +128,46 @@ public class Robot extends TimedRobot {
       conveyorMotorGroup.set(0.0);
     }
     
-    if (gamepad.getRawButton(6)){
+
+
+//**********COLOR WHEEL ROTATION CONTROL**********//
+//If top right bumper button is pressed, turn the color wheel drive motor
+    if (gamepadOperator.getRawButton(6)){
       colorWheelDrive.set(-1);
     }
     else{
-      //...oherwise turn it off.
+      //...otherwise turn it off.
       colorWheelDrive.set(0.0);
     }
+
+
+
+//**********COLOR WHEEL ARM CONTROL**********//
+//top left bumper button arms the color wheel mechanism 
+//bottom left trigger button retracts the color wheel mechanism (use limit switches to control)
+
+    //if top left bumper button is pressed and the upper limit switch is not pressed, raise the color wheel arm
+    if (gamepadOperator.getBumper(Hand.kLeft)){
+      //Check if colorWheelArmUpperLimit switch is not pressed before running motor
+      if(!colorWheelArmUpperLimit.get()) {
+        colorWheelArm.set(.5);
+    } else {
+      colorWheelArm.set(0);
+      }
+    }
+
+    if (gamepadOperator.getRawButton(2)){
+      //Check if colorWheelArmLowerLimit switch is not pressed before running motor
+      if(!colorWheelArmLowerLimit.get()) {
+        colorWheelArm.set(-.5);
+    } else {
+      colorWheelArm.set(0);
+      }
+    }
+
+
   }
+
   public double abs(double number){
     if(number > 0){
       return number;
@@ -116,27 +177,23 @@ public class Robot extends TimedRobot {
     }
   }
 }
+
+
 //on first gamepad:
 //left stick is dumper forward, right stick is intake forward
 //right joystick currently the one being used 
 //
 
-//on second gamepad
 
-//left button is full intake, bottom button is full stop, right button is full dump
+
 ///second operator is counting the cells to make sure there aren't more than 5
 
-//top left bumper button arms the color wheel mechanism 
-//bottom left bumper button retracts the color wheel mechanism (use limit switches to control)
-
 //top right bumper button rotates color wheel between 3 - 5 times
-//bottom right bumper button sets color to value from FMS
+//bottom right trigger button sets color to value from FMS
+
 //dashboard shows sequence complete to say that stage 1 color wheel is complete
 //read color output from FMS
 //D pad for hanging deploy sequence
 //safety button for deploy
 
 //use rumble feature to show that we are in position against the trench
-
-
-
