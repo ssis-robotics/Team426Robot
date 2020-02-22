78daa823109241cdd54c2afd95ea6e157af57d31
ZACTF426 <60770707+ZACTF426@users.noreply.github.com>
2020-02-22 17:45:36 -0500

    Update Robot.java
    Added simple autonomous.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,7 +14,7 @@ import edu.wpi.first.wpilibj.SpeedControllerGroup;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -24,6 +24,8 @@ import edu.wpi.first.wpilibj.DigitalInput;
 
 public class Robot extends TimedRobot {
   private DifferentialDrive m_myRobot;
+
+  private final Timer m_timer = new Timer();
   //Define controllers. 
   private XboxController gamepadDrive;
   private XboxController gamepadOperator;
@@ -166,6 +168,27 @@ public class Robot extends TimedRobot {
     }
 
 
+  }
+   /**
+   * This function is run once each time the robot enters autonomous mode.
+   */
+  @Override
+  public void autonomousInit() {
+    m_timer.reset();
+    m_timer.start();
+  }
+
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() {
+    // Drive for 2 seconds
+    if (m_timer.get() < 2.0) {
+      m_myRobot.arcadeDrive(0.5, 0.0); // drive forwards half speed
+    } else {
+      m_myRobot.stopMotor(); // stop robot
+    }
   }
 
   public double abs(double number){
