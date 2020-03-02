defdfcd78a2f06e53e761a21394dc5b815a2ac2c
NamVo230302 <61132317+NamVo230302@users.noreply.github.com>
2020-03-02 10:59:46 +0700

    Update Robot.java
    added automation for Stage 2 color wheel spin

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -194,7 +194,15 @@ private int numberOfColorChanges = 0;
 //**********COLOR WHEEL ROTATION CONTROL**********//
 //If top right bumper button is pressed, turn the color wheel drive motor
     if (gamepadOperator.getRawButton(6)){
-      colorWheelDrive.set(-1);
+      //Check that the number of color changes is enough to achieve rotation control
+      if (numberOfColorChanges > kColorChangesForStageTwo){
+        stageTwoComplete = true;
+        colorWheelDrive.set(-1);
+      }
+      else{
+        stageTwoComplete = false;
+        colorWheelDrive.set(0.0);
+      }
     }
     else{
       //...otherwise turn it off.
@@ -288,16 +296,6 @@ private int numberOfColorChanges = 0;
   }
   //Set the lastColorString equal to the current one so that we can check for changes on the next loop
   lastColorString = colorString;
-
-  //Check that the number of color changes is enough to achieve rotation control
-  if (numberOfColorChanges > kColorChangesForStageTwo){
-
-    stageTwoComplete = true;
-
-  }
-  else{
-    stageTwoComplete = false;
-    }
 
   //Set up the start button to reset the counter for color changes of the wheel.
   if(gamepadOperator.getStartButtonPressed()){
